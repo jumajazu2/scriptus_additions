cdf55e64ae0c6e1c57502c320ab2931a15b34d11
mirozahorak <mirozahorak@users.noreply.github.com>
2024-11-18 00:48:41 +0100

    added macos fixes

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import audio_session
 import just_audio
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
